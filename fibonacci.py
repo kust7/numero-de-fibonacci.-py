b56b25a1c9a5d6cd4b2189629c020812bbacca5c
kust7 <152316668+kust7@users.noreply.github.com>
2023-11-28 14:56:03 -0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,17 +1,18 @@
-
 def fibonacci(n):
-  fib_sequence = [0, 1]
+  fib_sequence = [1, 1]
 
   for i in range(2, n):
       next_term = fib_sequence[i-1] + fib_sequence[i-2]
       fib_sequence.append(next_term)
 
   return fib_sequence
-def main():  
+
+def main(): 
   n = int(input("Digite o número de termos desejado: "))
   result = fibonacci(n)
-  print(f"Os {n} primeiros termos da sequência de Fibonacci são: {result}")
-return 0
+  for term in result:
+      print(term)
+  return 0
 
 if __name__ == "__main__":
   main()
